bf8c0dbf0aad7fc652f8445da9be6627fda8c738
Linux Alışkanlığı <89791007+linux-aliskaligi@users.noreply.github.com>
2022-02-14 08:01:46 +0300

    Add files via upload

---
+++ W11-Education-Etkinlestirme.bat
@@ -0,0 +1,3 @@
+slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+slmgr.vbs /skms kms8.msguides.com
+slmgr.vbs /ato
---
+++ W11-Home-Etkinlestirme.bat
@@ -0,0 +1,3 @@
+slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+slmgr.vbs /skms kms8.msguides.com
+slmgr.vbs /ato
